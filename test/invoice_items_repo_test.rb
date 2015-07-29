7241638955446b76268cce5541e821943ea64a5e
Ryan Asensio <ryan.asensio@gmail.com>
2015-07-29 09:57:06 -0600

    implemented find_by and find_all_by with a hash of lambdas

--- test/invoice_items_repo_test.rb
+++ test/invoice_items_repo_test.rb
@@ -13,13 +13,13 @@ class InvoiceItemsRepoTest < MiniTest::Test
   end
 
   def test_returns_item_associated_with_an_invoice_item
-    skip
+
     engine = SalesEngine.new
     engine.startup
     engine.invoice_item_repo.map_data(InvoiceItem,'../test/fixtures/invoice_items.csv')
     engine.item_repo.map_data(Item,'../test/fixtures/items.csv')
 
-    assert_equal '75', engine.invoice_item_repo.item('10')
+    assert_equal '34018', engine.invoice_item_repo.item('10').unit_price
   end
 
 
--- test/repo_test.rb
+++ test/repo_test.rb
@@ -58,4 +58,62 @@ class RepoTest < MiniTest::Test
     assert_equal Item, repo.random.class
   end
 
+  def test_it_can_find_by_id_with_FIND_BY
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'../test/fixtures/invoice_items.csv')
+
+    assert_equal '1859', engine.invoice_item_repo.find_by(:id,'10').unit_price
+  end
+
+  def test_it_can_find_by_first_name
+    engine = SalesEngine.new
+    engine.startup
+    engine.customer_repo.map_data(Customer,'../test/fixtures/customers.csv')
+
+    assert_equal 'Kris', engine.customer_repo.find_by(:first_name,'Logan').last_name
+  end
+
+  def test_it_can_find_by_name
+    engine = SalesEngine.new
+    engine.startup
+    engine.merchant_repo.map_data(Merchant,'../test/fixtures/merchants.csv')
+
+    assert_equal '12', engine.merchant_repo.find_by(:name,'Kozey Group').id
+  end
+
+  def test_it_can_find_by_unit_price
+    engine = SalesEngine.new
+    engine.startup
+    engine.item_repo.map_data(Item,'../test/fixtures/items.csv')
+
+    assert_equal '4', engine.item_repo.find_by(:unit_price,'4291').id
+  end
+
+  def test_it_can_find_by_invoice_id
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'../test/fixtures/invoice_items.csv')
+
+    assert_equal '13', engine.invoice_item_repo.find_by(:invoice_id,'3').id
+  end
+
+  def test_it_can_find_by_credit_card_number
+    engine = SalesEngine.new
+    engine.startup
+    engine.transaction_repo.map_data(Transaction,'../test/fixtures/transactions.csv')
+
+    assert_equal '1', engine.transaction_repo.find_by(:credit_card_number,'4654405418249630').id
+  end
+
+  def test_it_finds_all_by_a_parameter
+    engine = SalesEngine.new
+    engine.startup
+    engine.item_repo.map_data(Item,'../test/fixtures/items.csv')
+
+    assert_equal 15, engine.item_repo.find_all_by(:merchant_id,'1').size
+  end
+
+
+
 end
