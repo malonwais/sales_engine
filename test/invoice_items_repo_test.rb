248a58629b077ca3808be72a3c9353b329973058
Ryan Asensio <ryan.asensio@gmail.com>
2015-07-28 18:58:30 -0600

    refactor repo with find_by_id function

--- test/invoice_items_repo_test.rb
+++ test/invoice_items_repo_test.rb
@@ -2,24 +2,25 @@ require_relative 'test_helper'
 
 class InvoiceItemsRepoTest < MiniTest::Test
 
-  # def test_returns_invoice_id_associated_with_an_invoice_item
-  #   engine = SalesEngine.new
-  #   engine.startup
-  #   engine.invoice_repo.map_data(Invoice,'../test/fixtures/invoices.csv')
-  #   engine.merchant_repo.map_data(Merchant,'../test/fixtures/merchants.csv')
-  #
-  #   assert_equal '10', engine.invoice_repo.merchant_id('20')
-  # end
-  # 
-  # def test_returns_invoice_associated_with_an_invoice_item
-  #   engine = SalesEngine.new
-  #   engine.startup
-  #   engine.invoice_repo.map_data(Invoice,'../test/fixtures/invoices.csv')
-  #   engine.merchant_repo.map_data(Merchant,'../test/fixtures/merchants.csv')
-  #
-  #   assert_equal 'Bechtelar, Jones and Stokes', engine.invoice_repo.merchant('20').name
-  # end
 
+  def test_returns_invoice_associated_with_an_invoice_item
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'../test/fixtures/invoice_items.csv')
+    engine.invoice_repo.map_data(Invoice,'../test/fixtures/invoices.csv')
+
+    assert_equal '75', engine.invoice_item_repo.invoice('10').merchant_id
+  end
+
+  def test_returns_item_associated_with_an_invoice_item
+    skip
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'../test/fixtures/invoice_items.csv')
+    engine.item_repo.map_data(Item,'../test/fixtures/items.csv')
+
+    assert_equal '75', engine.invoice_item_repo.item('10')
+  end
 
 
 end
--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -29,15 +29,6 @@ class InvoiceRepoTest < MiniTest::Test
     assert_equal 4, engine.invoice_repo.items('2').size
   end
 
-  def test_returns_correct_customer_id
-    engine = SalesEngine.new
-    engine.startup
-    engine.invoice_repo.map_data(Invoice,'../test/fixtures/invoices.csv')
-    engine.customer_repo.map_data(Customer,'../test/fixtures/customers.csv')
-
-    assert_equal '1', engine.invoice_repo.customer_id('2')
-  end
-
   def test_returns_customer_associated_with_an_invoice
     engine = SalesEngine.new
     engine.startup
@@ -45,15 +36,6 @@ class InvoiceRepoTest < MiniTest::Test
     engine.customer_repo.map_data(Customer,'../test/fixtures/customers.csv')
 
     assert_equal 'Joey', engine.invoice_repo.customer('2').first_name
-  end
-
-  def test_returns_merchant_id_associated_with_an_invoice
-    engine = SalesEngine.new
-    engine.startup
-    engine.invoice_repo.map_data(Invoice,'../test/fixtures/invoices.csv')
-    engine.merchant_repo.map_data(Merchant,'../test/fixtures/merchants.csv')
-
-    assert_equal '10', engine.invoice_repo.merchant_id('20')
   end
 
   def test_returns_merchant_associated_with_an_invoice
