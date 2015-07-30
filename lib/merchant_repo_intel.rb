42d5d18d6443cddd412a6d79d44b2352ef017ef1
Ryan Asensio <ryan.asensio@gmail.com>
2015-07-30 11:36:14 -0600

    first business i working

--- lib/merchant_repo_intel.rb
+++ lib/merchant_repo_intel.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MerchantRepoIntel < Repo
 
 
@@ -7,6 +8,12 @@ class MerchantRepoIntel < Repo
     end
   end
 
+  def group_merchant_with_invoice_ids(hash)
+    Hash[hash.map do |merch, invoices|
+      [merch, invoices.map {|invoice| invoice.id}]
+    end]
+  end
+
   def group_invoice_items_with_invoice
     repo_table(:invoice_item_repo).group_by do |invoice_item|
       invoice_item.invoice_id
@@ -14,13 +21,58 @@ class MerchantRepoIntel < Repo
   end
 
   def total_inv_item_price(i_item)
-    i_item.unitprice * i_item.quantity
+    i_item.unit_price.to_i * i_item.quantity.to_i
   end
 
-  def total_invoice_prices(inv_to_inv_items)
-    invoice_prices = Hash[inv_to_inv_items.map do |inv, inv_item|
-      [inv, total_inv_item_price(inv_item) ]
+  def array_of_inv_item_prices(array)
+    array.map {|item| total_inv_item_price(item)}
+  end
 
+  def inv_to_inv_item_prices(inv_to_inv_items)
+    Hash[inv_to_inv_items.map do |inv, inv_item_array|
+      [inv, array_of_inv_item_prices(inv_item_array) ]
+    end]
+  end
+
+  def map_merchants_to_invoice_prices(merch_hash, invoice_price_hash)
+    Hash[merch_hash.map do |merch_id, invoice_ids|
+      [merch_id, invoice_ids.map {|id| invoice_price_hash[id] ||= 0 }]
+    end]
+  end
+
+  def add_array_in_hash(hash)
+    Hash[hash.map do |key, array_of_things|
+      [key, array_of_things.inject(:+)]
+    end]
+  end
+
+  def most_revenue(top_x)
+    sep_prices = inv_to_inv_item_prices(group_invoice_items_with_invoice)
+    total_inv_price = add_array_in_hash(sep_prices)
+    merch_with_ids = group_merchant_with_invoice_ids(group_invoices_with_merchant)
+    merch_with_spends = map_merchants_to_invoice_prices(merch_with_ids, total_inv_price)
+    total_spends_per_merch = add_array_in_hash(merch_with_spends)
+    merch_spend = []
+    total_spends_per_merch.each do |merch, spend|
+      merch_spend << [merch, spend]
+    end
+    merch_spend.sort_by {|a| a[1]}.last(top_x).reverse
+  end
+
+  def inv_item_quantities(array)
+    array.map { |item| item.quantity }
+  end
+
+  def invoice_item_quantity_with_invoice(inv_with_inv_itmes)
+    Hash[inv_with_inv_itmes.map do |inv_id, inv_items|
+      [inv_id, inv_item_quantities(inv_items)]
+    end]
+  end
+
+  def most_items(top_x)
+    group_merchant_with_invoice_ids(group_invoices_with_merchant)
+
+    group_invoice_items_with_invoice
 
   end
 
--- test/merchant_repo_intel_test.rb
+++ test/merchant_repo_intel_test.rb
@@ -19,6 +19,18 @@ class MerchantRepoIntelTest < MiniTest::Test
     assert_equal 2, result.size
   end
 
+  def test_can_group_merchant_with_invoice_id
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_repo.map_data(Invoice,'test/fixtures/invoices.csv')
+    intel = MerchantRepoIntel.new(engine)
+    int = intel.group_invoices_with_merchant
+    result = intel.group_merchant_with_invoice_ids(int)
+
+    assert_equal ['7','14'], result['44']
+  end
+
+
   def test_can_group_invoice_items_by_merchant
     engine = SalesEngine.new
     engine.startup
@@ -28,6 +40,129 @@ class MerchantRepoIntelTest < MiniTest::Test
 
     assert_equal 4, result.size
   end
+
+  def test_can_calulate_toal_price
+    engine = SalesEngine.new
+    engine.startup
+    item = InvoiceItem.new([1,2,3,'4','5',5,6])
+    intel = MerchantRepoIntel.new(engine)
+    result = intel.total_inv_item_price(item)
+
+    assert_equal 20, result
+  end
+
+  def test_can_calulate_toal_price_of_an_array
+    engine = SalesEngine.new
+    engine.startup
+    item1 = InvoiceItem.new([1,2,3,'4','5',5,6])
+    item2 = InvoiceItem.new([1,2,3,'5','6',5,6])
+    intel = MerchantRepoIntel.new(engine)
+
+    result = intel.array_of_inv_item_prices([item1, item2])
+
+    assert_equal 30, result[1]
+  end
+
+
+  def test_can_find_total_inv_item_prices_for_an_invoice
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    intel = MerchantRepoIntel.new(engine)
+    inv_items = intel.group_invoice_items_with_invoice
+    result = intel.inv_to_inv_item_prices(inv_items)
+
+    assert_equal 68175, result['1'].first
+  end
+
+  def test_can_find_total_invoice_price
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    intel = MerchantRepoIntel.new(engine)
+    inv_items = intel.group_invoice_items_with_invoice
+    sep_prices = intel.inv_to_inv_item_prices(inv_items)
+    total_inv_price = intel.add_array_in_hash(sep_prices)
+
+    assert_equal 2106777, total_inv_price['1']
+  end
+
+  def test_can_map_merch_to_invoice_prices
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    engine.invoice_repo.map_data(Invoice,'test/fixtures/invoices.csv')
+    intel = MerchantRepoIntel.new(engine)
+
+    inv_items = intel.group_invoice_items_with_invoice
+    sep_prices = intel.inv_to_inv_item_prices(inv_items)
+    total_inv_price = intel.add_array_in_hash(sep_prices)
+
+    int = intel.group_invoices_with_merchant
+    merch_with_ids = intel.group_merchant_with_invoice_ids(int)
+    result = intel.map_merchants_to_invoice_prices(merch_with_ids, total_inv_price)
+    assert_equal 2106777, result['26'].first
+  end
+
+  def test_can_find_total_spend_per_merchant
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    engine.invoice_repo.map_data(Invoice,'test/fixtures/invoices.csv')
+    intel = MerchantRepoIntel.new(engine)
+
+    inv_items = intel.group_invoice_items_with_invoice
+    sep_prices = intel.inv_to_inv_item_prices(inv_items)
+    total_inv_price = intel.add_array_in_hash(sep_prices)
+
+    int = intel.group_invoices_with_merchant
+    merch_with_ids = intel.group_merchant_with_invoice_ids(int)
+    merch_with_spends = intel.map_merchants_to_invoice_prices(merch_with_ids, total_inv_price)
+    result = intel.add_array_in_hash(merch_with_spends)
+
+    assert_equal 2106777, result['26']
+  end
+
+  def test_can_find_top_x_merch_by_revenue
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    engine.invoice_repo.map_data(Invoice,'test/fixtures/invoices.csv')
+    intel = MerchantRepoIntel.new(engine)
+
+    result = intel.most_revenue(2)
+    assert_equal [["78", 2367699], ["26", 2106777]], result
+  end
+
+  def test_can_convert_i_items_to_quantities
+    engine = SalesEngine.new
+    engine.startup
+    item1 = InvoiceItem.new([1,2,3,'4','5',5,6])
+    item2 = InvoiceItem.new([1,2,3,'5','6',5,6])
+    intel = MerchantRepoIntel.new(engine)
+    result = intel.inv_item_quantities([item1, item2])
+
+    assert_equal ['4','5'], result
+  end
+
+  def test_can_map_inv_item_quantities_to_invoice_id
+    engine = SalesEngine.new
+    engine.startup
+    engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
+    engine.invoice_repo.map_data(Invoice,'test/fixtures/invoices.csv')
+    intel = MerchantRepoIntel.new(engine)
+
+    inv_items = intel.group_invoice_items_with_invoice
+    sep_prices = intel.invoice_item_quantity_with_invoice(inv_items)
+    total_inv_price = intel.add_array_in_hash(sep_prices)
+
+    int = intel.group_invoices_with_merchant
+    merch_with_ids = intel.group_merchant_with_invoice_ids(int)
+    merch_with_spends = intel.map_merchants_to_invoice_prices(merch_with_ids, total_inv_price)
+    result = intel.add_array_in_hash(merch_with_spends)
+
+  end
+
 
 
 
