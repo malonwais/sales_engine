ed101bf74fd27d3db41c7aeb522ff83dbe6e030d
Alon Waisman <mowalon@yahoo.com>
2015-08-04 21:59:51 -0600

    completed invoice_repo.create

--- lib/invoice_item_repo.rb
+++ lib/invoice_item_repo.rb
@@ -43,4 +43,22 @@ class InvoiceItemRepository < Repo
     output
   end
 
+  def add_invoice_items(items, invoice_id)
+    items_by_id = items.group_by {|item| item.id}
+    item_counts = items_by_id.map{|id, items| [id, items.count]}
+
+    item_counts.each do |item_id, quantity|
+      new_id = table[-1].id + 1
+      unit_price = se.item_repository.find_by(:id, item_id).unit_price
+      created_at = Time.now.utc.to_s
+      updated_at = created_at
+
+      new_record = [new_id, item_id, invoice_id, quantity, unit_price, created_at, updated_at]
+      new_invoice_item = InvoiceItem.new(new_record, self)
+      table << new_invoice_item
+      hash = populate_hash(table)
+    end
+  end
+
+
 end
