47e988916d3a246a8446a89760575d16efafe605
HoffsMH <hecker.matthew@gmail.com>
2015-08-05 17:54:58 -0600

    about to merge master again

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -10,16 +10,16 @@ class InvoiceItemTest < MiniTest::Test
   end
 
   def test_it_can_pull_an_invoice
-    invoice_item = engine.invoice_item_repository.find_by(:id, '13')
+    invoice_item = engine.invoice_item_repository.find_by(:id, 13)
 
     assert_equal Invoice, invoice_item.invoice.class
   end
 
   def test_it_pulls_the_correct_invoice
-    invoice_item = engine.invoice_item_repository.find_by(:id, '13')
+    invoice_item = engine.invoice_item_repository.find_by(:id, 13)
 
-    assert_equal '3', invoice_item.invoice.id
-  	assert_equal '78', invoice_item.invoice.merchant_id
+    assert_equal 3, invoice_item.invoice.id
+  	assert_equal 78, invoice_item.invoice.merchant_id
   end
 
     def test_it_returns_nil_when_invoice_is_not_found
@@ -30,16 +30,16 @@ class InvoiceItemTest < MiniTest::Test
     end
 
   def test_it_can_pull_an_item
-    invoice_item = engine.invoice_item_repository.find_by(:id, '8')
+    invoice_item = engine.invoice_item_repository.find_by(:id, 8)
 
     assert_equal Item, invoice_item.item.class
   end
 
   def test_it_pulls_the_correct_item
-    invoice_item = engine.invoice_item_repository.find_by(:id, '8')
+    invoice_item = engine.invoice_item_repository.find_by(:id, 8)
 
-    assert_equal '534', invoice_item.item.id
-    assert_equal '76941', invoice_item.item.unit_price
+    assert_equal 534, invoice_item.item.id
+    assert_equal 76941, invoice_item.item.unit_price
   end
 
   def test_it_returns_nil_when_item_is_not_found
