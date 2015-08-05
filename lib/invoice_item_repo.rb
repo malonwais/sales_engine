05d22db00d35e70d498d5661c74ad1c0dd0bf0f1
Alon Waisman <mowalon@yahoo.com>
2015-08-04 18:30:26 -0600

    best_day unfinished

--- lib/invoice_item_repo.rb
+++ lib/invoice_item_repo.rb
@@ -10,46 +10,36 @@ class InvoiceItemRepository < Repo
     @invoice_items =  @table
     map_data(InvoiceItem,'../sales_engine/data/invoice_items.csv')
   end
-  
+
   def item_data_by_invoice(method_name)
     output_hash = {}
-    
+
     se.invoice_item_repository.table.each do |invoice_item|
       invoice_id = invoice_item.invoice_id
       item_id = invoice_item.item_id
       data = invoice_item.send(method_name)
       output_hash[invoice_id] ||= {}
-      
+
       if output_hash[invoice_id][item_id].nil?
         output_hash[invoice_id][item_id] = data
       else
         output_hash[invoice_id][item_id] += data
       end
-      
+
     end
     output_hash
   end
-  
-  
+
+
   def items_values(data_by_invoice)
-    output = {}
+    output = Hash.new(0)
     data_by_invoice.each do |invoice_id, items_with_values|
       invoice = se.invoice_repository.find_by_id(invoice_id)
       if invoice.successful?
-        items_with_values.each do |item_id, value|
-          if output[item_id].nil?
-            output[item_id] = value
-          else
-            output[item_id] += value
-          end
-        end
-        
+        items_with_values.each {|item_id, value| output[item_id] += value}
       end
     end
     output
-    
   end
-
-
 
 end
