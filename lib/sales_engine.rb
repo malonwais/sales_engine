6e13c85d9805557fbf187a532cc07cbbae64f696
HoffsMH <hecker.matthew@gmail.com>
2015-08-05 14:37:28 -0600

    cleaned up customer.rb

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -11,16 +11,15 @@ require 'pry'
 class SalesEngine
   attr_reader :customer_repository, :invoice_repository, :transaction_repository,
   :invoice_item_repository, :merchant_repository, :item_repository, :csv_path
-  
+
   def initialize(csv_path=our_folder)
     @csv_path = csv_path
-    
+
   end
   def our_folder
     our_root = File.expand_path('../..',  __FILE__)
     File.join our_root, "data"
   end
-  
   def repository_startup
     @customer_repository = CustomerRepository.new(self, csv_path)
     @invoice_repository = InvoiceRepository.new(self, csv_path)
@@ -32,7 +31,7 @@ class SalesEngine
   def startup
     repository_startup
   end
-  
+
 end
 
 engine = SalesEngine.new()
