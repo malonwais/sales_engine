376e299647c6229c025b66a2d2002a1131f131a4
HoffsMH <hecker.matthew@gmail.com>
2015-08-05 21:42:20 -0600

    still fixing

--- test/item_test.rb
+++ test/item_test.rb
@@ -10,7 +10,7 @@ class ItemTest < MiniTest::Test
     end
 
     def test_invoice_items__it_returns_an_array_of_invoice_items
-      item = engine.item_repository.find_by(:id, '127')
+      item = engine.item_repository.find_by(:id, 127)
 
       assert_equal Array, item.invoice_items.class
       assert item.invoice_items.all?{|invoice_item| invoice_item.class == InvoiceItem}
@@ -18,12 +18,12 @@ class ItemTest < MiniTest::Test
 
 
     def test_invoice_items__it_returns_the_correct_invoice_items
-      item = engine.item_repository.find_by(:id, '127')
+      item = engine.item_repository.find_by(:id, 127)
       invoice_item_ids = item.invoice_items.map {|invoice_item| invoice_item.id}
       quantities = item.invoice_items.map {|invoice_item| invoice_item.quantity}
 
-      assert_equal ['57', '60'], invoice_item_ids[0..1]
-      assert_equal ['2', '3'], quantities[0..1]
+      assert_equal [21229, 13497], invoice_item_ids[0..1]
+      assert_equal [4, 1], quantities[0..1]
     end
 
     def test_invoice_items__it_returns_an_empty_array_when_no_invoice_items_are_associated_with_item
@@ -34,48 +34,15 @@ class ItemTest < MiniTest::Test
     end
 
     def test_merchant__it_can_pull_a_merchant
-      item = engine.item_repository.find_by(:id, '127')
+      item = engine.item_repository.find_by(:id, 127)
 
       assert_equal Merchant, item.merchant.class
     end
 
     def test_merchant__it_pulls_the_correct_merchant
-      item = engine.item_repository.find_by(:id, '127')
+      item = engine.item_repository.find_by(:id, 127)
 
-      assert_equal "8", item.merchant.id
+      assert_equal 8, item.merchant.id
       assert_equal "Osinski, Pollich and Koelpin", item.merchant.name
     end
-    def test_get_invoice_item_quantity_it_returns_0_if_item_doesnt_exist
-
-      input_data = ['091820193','hi','this is','234245', '23423424','2342342342','232523525']
-      item = Item.new(input_data, engine.item_repository)
-
-      assert_equal(0, item.get_invoice_item_quantity)
-    end
-
-
-
-
-
-
-
-  # def test_can_find_invoice_items
-  #   engine = SalesEngine.new
-  #   engine.startup
-  #   engine.invoice_item_repo.map_data(InvoiceItem,'test/fixtures/invoice_items.csv')
-  #   engine.item_repo.map_data(Item,'test/fixtures/items.csv')
-  #
-  #   assert_equal 2, engine.item_repo.invoice_items('1').size
-  # end
-  #
-  # def test_can_find_the_associated_merchant
-  #   engine = SalesEngine.new
-  #   engine.startup
-  #   engine.merchant_repo.map_data(Merchant,'test/fixtures/merchants.csv')
-  #   engine.item_repo.map_data(Item,'test/fixtures/items.csv')
-  #
-  #   assert_equal 'Klein, Rempel and Jones', engine.item_repo.merchant('16').name
-  # end
-
-
 end
